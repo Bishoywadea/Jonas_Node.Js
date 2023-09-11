314508e75a1c9d27197d8cd118f8af1fac2710a6
Bishoywadea <108888519+Bishoywadea@users.noreply.github.com>
2023-09-11 16:22:28 +0300

    make async callBacks to readFile

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,7 +1,24 @@
 const fs = require('fs');
 
+// Blocking, synchtonous way
 const textIn = fs.readFileSync('./txt/input.txt','utf-8');
 console.log(textIn);
-
 const textOut = `this is the avoooocado: ${textIn} \non ${Date.now()}`;
 fs.writeFileSync('./txt/output.txt',textOut);
+
+// NonBlocking, asynchtonous way
+// CallBack HELL
+fs.readFile('./txt/start.txt','utf-8', (err,data) =>{
+    if(err) return console.log('cant find start.txt 💩');
+    fs.readFile(`./txt/${data}.txt`,'utf-8', (err,data2) =>{
+        console.log(data2);
+        fs.readFile(`./txt/append.txt`,'utf-8', (err,data3) =>{
+            console.log(data3);
+            fs.writeFile('./txt/final.txt',`${data2}\n${data3}`,'utf-8',err =>{
+                console.log('the file is written 👌');
+            });
+        });
+    });
+});
+console.log('async');
+
