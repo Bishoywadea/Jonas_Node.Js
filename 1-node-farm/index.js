a460acae0429bc180c13b3629bed19bebe3994d1
Bishoywadea <108888519+Bishoywadea@users.noreply.github.com>
2023-09-13 07:02:35 +0300

    adding some dep to the code

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
-const http  = require('http');
-const url  = require('url');
+const http = require('http');
+const url = require('url');
+const slugify = require('slugify');
+const replaceTemplate = require('./modules/replaceTemplate')
 
 ///////////////////////////////////////////
 ////////////Files Handling/////////////////
@@ -33,30 +35,17 @@ console.log('async');
 ////////////Servers////////////////////////
 ///////////////////////////////////////////
 
-const replaceTemplate = (temp,product) => {
-    // </{%PRODUCTNAME%}/g> this means to replace all the place holders not the first one
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    return output;
-}
-
 // the "." means the directory we are running the script from
 // the "__dirname" means the current directory of the file
- /* we used the sync version because it will be executed one time 
+/* we used the sync version because it will be executed one time 
  in the beginning instead of executed each time a response come  */
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map(el=>slugify(el.productName,{lower:true}));
 
 const server = http.createServer((request,response)=>{
     //Note: Express is tool to handle complex routes in big project
