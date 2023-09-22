2e61d8b8a4a9ea539e86ec25bc98f43cfbe07db7
Bishoywadea <108888519+Bishoywadea@users.noreply.github.com>
2023-09-22 10:36:57 +0300

    add mongoose package to the app

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,8 +1,12 @@
 const print = console.log;
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+mongoose.connect(DB, {
+}).then(() => print('DB connection is Done'));
 // print(process.env);
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
